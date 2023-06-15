af379c44a71fbe4b678bd8c808b0618c105e94fa
Jon <lundbyjon@hotmail.com>
2023-06-16 01:48:49 +0200

    CSS input output text areas and encode btn. JS seperated textConvert from startApp and calls on submit

--- script.js
+++ script.js
@@ -2,44 +2,61 @@
 
 const alphabetStr = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let alphabetArr = [];
-let userTextStr = "the rat is in the house";
-let userTextArr = [];
-let rotationNum = -4;
-let encryptionArr = [];
-let encryptedLetter = "";
-let encryptedText = "";
+//let userTextStr = "the rat is in the house";
+//let userTextArr = [];
+let rotationNum;
 
 window.addEventListener("load", startApp);
 
 function startApp() {
-    console.log("running");
+    //making an alphabet array from alphabetStr
+    alphabetArr = alphabetStr.split("");    
+
+    //eventlistener for encode btn.
+    document.querySelector("#user-text-form").addEventListener("submit", convertText);
+
+    //eventlistener for rotation number
+    document.querySelector("#rotationNumber").addEventListener("change", setRotationNum);
     
-    alphabetArr = alphabetStr.split("");
+}
+
+
+function setRotationNum(event) {
+    console.log("rotnoum called")
+    rotationNum = event.target.rotationNumber.value;
+    console.log(rotationNum)
+}
+
+function convertText(event) {
+    event.preventDefault();
+    
+    const form = event.target;
+    const userTextStr = form.userText.value;
+    let userTextArr = [];
+    let encryptionArr = [];
+    let encryptedLetter = "";
+    let encryptedText = "";
+    
     userTextArr = userTextStr.split("");
 
-    console.log(alphabetArr.length);
-
     for (let i = 0; i < userTextArr.length; i++) {
-        const letterUserTxt = userTextArr[i];
-        if (letterUserTxt === " ") {
-            // encryptedLetter = letterUserTxt;
-            encryptionArr.push(letterUserTxt);
+      const letterUserTxt = userTextArr[i];
+      if (letterUserTxt === " " || letterUserTxt === ".") {
+        // encryptedLetter = letterUserTxt;
+        encryptionArr.push(letterUserTxt);
+      }
+
+      for (let j = 0; j < alphabetArr.length; j++) {
+        const letterAlphabet = alphabetArr[j];
+
+        if (letterUserTxt === letterAlphabet) {
+          encryptedLetter = alphabetArr[j + rotationNum];
+          encryptionArr.push(encryptedLetter);
         }
-        
-        for (let j = 0; j < alphabetArr.length; j++) {
-            const letterAlphabet = alphabetArr[j];
-
-            if (letterUserTxt === letterAlphabet) {
-                encryptedLetter = alphabetArr[(j + rotationNum)]
-                console.log(encryptedLetter);
-                encryptionArr.push(encryptedLetter);
-            }
-
-        }
-
+      }
     }
 
-    
     encryptedText = encryptionArr.join(``);
-    console.log(encryptedText)
-} 
+    // console.log(encryptedText);
+    document.querySelector("#encodedText").textContent = encryptedText;
+}
