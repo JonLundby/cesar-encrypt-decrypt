b32cbb76323909a491a607cd84266f38b03bff9b
Jon <lundbyjon@hotmail.com>
2023-06-19 15:03:30 +0200

    cesar -0 - -9 encryption working with a "full circle" alphabet

--- script.js
+++ script.js
@@ -4,59 +4,66 @@ const alphabetStr = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let alphabetArr = [];
 //let userTextStr = "the rat is in the house";
 //let userTextArr = [];
-let rotationNum;
+let rotationNum = 0;
 
 window.addEventListener("load", startApp);
 
 function startApp() {
-    //making an alphabet array from alphabetStr
-    alphabetArr = alphabetStr.split("");    
+  //making an alphabet array from alphabetStr
+  alphabetArr = alphabetStr.split("");
 
-    //eventlistener for encode btn.
-    document.querySelector("#user-text-form").addEventListener("submit", convertText);
+  //eventlistener for encode btn.
+  document.querySelector("#user-text-form").addEventListener("submit", convertText);
 
-    //eventlistener for rotation number
-    document.querySelector("#rotationNumber").addEventListener("change", setRotationNum);
-    
+  //eventlistener for rotation number
+  document.querySelector("#rotation-number").addEventListener("change", setRotationNum);
 }
 
-
 function setRotationNum(event) {
-    console.log("rotnoum called")
-    rotationNum = event.target.rotationNumber.value;
-    console.log(rotationNum)
+  // console.log("rotnoum called");
+  const value = event.target.value;
+  rotationNum = value;
+  // console.log(rotationNum);
 }
 
 function convertText(event) {
-    event.preventDefault();
-    
-    const form = event.target;
-    const userTextStr = form.userText.value;
-    let userTextArr = [];
-    let encryptionArr = [];
-    let encryptedLetter = "";
-    let encryptedText = "";
-    
-    userTextArr = userTextStr.split("");
+  event.preventDefault();
 
-    for (let i = 0; i < userTextArr.length; i++) {
-      const letterUserTxt = userTextArr[i];
-      if (letterUserTxt === " " || letterUserTxt === ".") {
-        // encryptedLetter = letterUserTxt;
-        encryptionArr.push(letterUserTxt);
-      }
+  const form = event.target;
+  const userTextStr = form.userText.value;
+  let userTextArr = [];
+  let encryptionArr = [];
+  let encryptedLetter = "";
+  let encryptedText = "";
+  userTextArr = userTextStr.split("");
 
-      for (let j = 0; j < alphabetArr.length; j++) {
-        const letterAlphabet = alphabetArr[j];
-
-        if (letterUserTxt === letterAlphabet) {
-          encryptedLetter = alphabetArr[j + rotationNum];
-          encryptionArr.push(encryptedLetter);
-        }
-      }
+  //iterating over each letter of the user text array
+  for (let i = 0; i < userTextArr.length; i++) {
+    const letterUserTxt = userTextArr[i];
+    if (letterUserTxt === " " || letterUserTxt === ".") {
+      // encryptedLetter = letterUserTxt;
+      encryptionArr.push(letterUserTxt);
     }
 
-    encryptedText = encryptionArr.join(``);
-    // console.log(encryptedText);
-    document.querySelector("#encodedText").textContent = encryptedText;
+    //iterating over each letter in the alphabet and checking if it is equal to the given iterated letter of the user text
+    for (let j = 0; j < alphabetArr.length; j++) {
+      const letterAlphabet = alphabetArr[j];
+      let calibrationNum = alphabetArr.length;
+
+      //pushing encryption and checking if rotationNum pushes letterUserTxt out of alphabetArr range
+      if (letterUserTxt === letterAlphabet && j - rotationNum < 0) {
+        calibrationNum = calibrationNum - Math.abs(j - rotationNum);
+        encryptedLetter = alphabetArr[calibrationNum];
+        encryptionArr.push(encryptedLetter);
+      } else if (letterUserTxt === letterAlphabet) {
+        encryptedLetter = alphabetArr[j - rotationNum];
+        encryptionArr.push(encryptedLetter);
+      }
+    }
+  }
+
+  //sending encrypted text to "#encodedText"
+  encryptedText = encryptionArr.join(``);
+  console.log(encryptedText);
+  document.querySelector("#encodedText").textContent = encryptedText;
 }
